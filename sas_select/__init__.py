6c0c3ccde5d141ba87b6e51ec629675597b70f1d
D Tim Cummings <tim@triptera.com.au>
2019-07-14 14:55:51 +1000

    fixing issue #13 : pack_entitlement() not leaving spaces in result str
    Pull request from anniequasar applied as a patch.

--- sas_select/__init__.py
+++ sas_select/__init__.py
@@ -79,12 +79,12 @@ def create_app(test_config=None):
         packs = int(entitlement[:-1])//packsize
         exception1 = packsize//int(entitlement[:-1])
         if packs > 0:
-            return "Your medicare entitlement is" + str(packs) + "packs per" + frequency
+            return "Your medicare entitlement is " + str(packs) + " packs per " + frequency
         else:
             remainder = packsize % int(entitlement[:-1])
             if remainder > 0:
                 exception1 += 1
-        return "Your medicare entitlement is" + str(exception1) + "pack per" + frequency
+        return "Your medicare entitlement is " + str(exception1) + " packs per " + frequency
 
     @app.route('/product/<id_product>')
     def view_product(id_product):
